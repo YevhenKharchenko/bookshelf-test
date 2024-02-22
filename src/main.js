d8f9e36eeec958d5c1be841d70aee68e5d0f4e33
YevhenKharchenko <evgenvalerych@gmail.com>
2024-02-22 14:56:24 +0200

    render added

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { BooksApi } from './js/books-api';
 
 const booksApi = new BooksApi();
 
-// const hardcoverFiction = await booksApi.getCategory('Hardcover Nonfiction');
-// const book = await booksApi.getBook('643282b1e85766588626a0c0');
 const booksList = document.querySelector('.books-list');
 const topBooksList = document.querySelector('.top-books-list');
+const categoryList = document.querySelector('.category');
+const bookItem = document.querySelector('.book-item');
 
 async function renderBooksList() {
   const categories = await booksApi.getCategories();
@@ -40,5 +40,36 @@ async function renderTopBooks() {
   });
 }
 
+async function renderCategory(category) {
+  const booksList = await booksApi.getCategory(category);
+  const markup = booksList
+    .map(book => {
+      return `<div class="book" id="${book._id}">
+            <img src="${book.book_image}" alt="${book.title}">
+            <h3>${book.title}</h3>
+            <p>Author: ${book.author}</p>
+            <p>Publisher: ${book.publisher}</p>
+            <a href="${book.amazon_product_url}" target="_blank">Buy on Amazon</a>
+        </div>`;
+    })
+    .join('');
+
+  categoryList.insertAdjacentHTML('beforeend', markup);
+}
+
+async function renderBook(id) {
+  const book = await booksApi.getBook(id);
+
+  const markup = `<img src="${book.book_image}" alt="${book.title}">
+                  <h3>${book.title}</h3>
+                  <p>Author: ${book.author}</p>
+                  <p>Publisher: ${book.publisher}</p>
+                  <a href="${book.amazon_product_url}" target="_blank">Buy on Amazon</a>`;
+
+  bookItem.insertAdjacentHTML('beforeend', markup);
+}
+
 renderBooksList();
 renderTopBooks();
+renderCategory('Picture Books');
+renderBook('643282b2e85766588626a10c');
