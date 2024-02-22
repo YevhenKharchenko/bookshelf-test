d7028c4642675795624c8233f98f0bac7ceecbe3
YevhenKharchenko <143255199+YevhenKharchenko@users.noreply.github.com>
2024-02-22 15:42:59 +0000

    Deploying to gh-pages from @ YevhenKharchenko/bookshelf-test@0cffb0c92f9a9dc45afdc334215173164ae4478a 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,19 +1,15 @@
-import{a as i}from"./assets/vendor-0cb09735.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))r(o);new MutationObserver(o=>{for(const e of o)if(e.type==="childList")for(const c of e.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&r(c)}).observe(document,{childList:!0,subtree:!0});function s(o){const e={};return o.integrity&&(e.integrity=o.integrity),o.referrerPolicy&&(e.referrerPolicy=o.referrerPolicy),o.crossOrigin==="use-credentials"?e.credentials="include":o.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function r(o){if(o.ep)return;o.ep=!0;const e=s(o);fetch(o.href,e)}})();class u{constructor(){this.BASE_URL="https://books-backend.p.goit.global/books",this.CATEGORY_LIST="/category-list",this.TOP_BOOKS="/top-books",this.CATEGORY="/category?category="}async getCategories(){const t=this.BASE_URL+this.CATEGORY_LIST;return(await i.get(t)).data}async getTopBooks(){const t=this.BASE_URL+this.TOP_BOOKS;return(await i.get(t)).data}async getCategory(t){const s=this.BASE_URL+this.CATEGORY+t;return(await i.get(s)).data}async getBook(t){const s=this.BASE_URL+"/"+t;return(await i.get(s)).data}}const a=new u,l=document.querySelector(".books-list"),p=document.querySelector(".top-books-list"),d=document.querySelector(".category"),g=document.querySelector(".book-item");async function h(){const t=(await a.getCategories()).map(({list_name:s})=>`<li>${s}</li>`).join("");l.insertAdjacentHTML("beforeend",t)}async function f(){(await a.getTopBooks()).forEach(t=>{const s=`<h2>${t.list_name}</h2>`,r=t.books.map(e=>`
-        <div class="book" id="${e._id}">
+import{a as i}from"./assets/vendor-0cb09735.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))e(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const a of o.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&e(a)}).observe(document,{childList:!0,subtree:!0});function s(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function e(t){if(t.ep)return;t.ep=!0;const o=s(t);fetch(t.href,o)}})();class u{constructor(){this.BASE_URL="https://books-backend.p.goit.global/books",this.CATEGORY_LIST="/category-list",this.TOP_BOOKS="/top-books",this.CATEGORY="/category?category="}async getCategories(){const r=this.BASE_URL+this.CATEGORY_LIST;return(await i.get(r)).data}async getTopBooks(){const r=this.BASE_URL+this.TOP_BOOKS;return(await i.get(r)).data}async getCategory(r){const s=this.BASE_URL+this.CATEGORY+r;return(await i.get(s)).data}async getBook(r){const s=this.BASE_URL+"/"+r;return(await i.get(s)).data}}const c=new u,d=document.querySelector(".categories-list"),l=document.querySelector(".category");document.querySelector(".book-item");async function p(){const r=(await c.getCategories()).map(({list_name:e})=>`<a class="category-link" href=""><li>${e}</li></a>`).join("");d.insertAdjacentHTML("beforeend",r),document.querySelectorAll(".category-link").forEach(e=>e.addEventListener("click",t=>{t.preventDefault(),l.innerHTML="",f(e.textContent)}))}async function g(){(await c.getTopBooks()).forEach(r=>{const s=`<h2>${r.list_name}</h2>`,e=r.books.map(o=>`
+        <div class="book" id="${o._id}">
+            <img src="${o.book_image}" alt="${o.title}">
+            <h3>${o.title}</h3>
+            <p>Author: ${o.author}</p>
+            <p>Publisher: ${o.publisher}</p>
+            <a href="${o.amazon_product_url}" target="_blank">Buy on Amazon</a>
+        </div>`).join(""),t=s+e;l.insertAdjacentHTML("beforeend",t)})}async function f(n){const s=(await c.getCategory(n)).map(e=>`<div class="book" id="${e._id}">
             <img src="${e.book_image}" alt="${e.title}">
             <h3>${e.title}</h3>
             <p>Author: ${e.author}</p>
             <p>Publisher: ${e.publisher}</p>
             <a href="${e.amazon_product_url}" target="_blank">Buy on Amazon</a>
-        </div>`).join(""),o=s+r;p.insertAdjacentHTML("beforeend",o)})}async function m(n){const s=(await a.getCategory(n)).map(r=>`<div class="book" id="${r._id}">
-            <img src="${r.book_image}" alt="${r.title}">
-            <h3>${r.title}</h3>
-            <p>Author: ${r.author}</p>
-            <p>Publisher: ${r.publisher}</p>
-            <a href="${r.amazon_product_url}" target="_blank">Buy on Amazon</a>
-        </div>`).join("");d.insertAdjacentHTML("beforeend",s)}async function y(n){const t=await a.getBook(n),s=`<img src="${t.book_image}" alt="${t.title}">
-                  <h3>${t.title}</h3>
-                  <p>Author: ${t.author}</p>
-                  <p>Publisher: ${t.publisher}</p>
-                  <a href="${t.amazon_product_url}" target="_blank">Buy on Amazon</a>`;g.insertAdjacentHTML("beforeend",s)}h();f();m("Picture Books");y("643282b2e85766588626a10c");
+        </div>`).join("");l.insertAdjacentHTML("beforeend",s)}p();g();
 //# sourceMappingURL=commonHelpers.js.map
