c082f2646629a743f72b06c2e8dad81c83e5b3e2
YevhenKharchenko <143255199+YevhenKharchenko@users.noreply.github.com>
2024-02-27 10:51:28 +0000

    Deploying to gh-pages from @ YevhenKharchenko/bookshelf-test@eb355dde4eff4a9cecddd71bb33a3aec3f192f76 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,11 +1,11 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                             */import{P as s}from"./assets/vendor-e8675f53.js";const c=document.getElementById("tui-pagination-container"),p=new s(c);p.getCurrentPage();const n=document.querySelector(".shopping-list"),o=JSON.parse(localStorage.getItem("books"))||[];function l(){try{const t=JSON.parse(localStorage.getItem("books"))||[];if(!t)return;t.forEach(e=>g(e))}catch(t){console.log(t),iziToast.error({title:"Error",message:`Oops! Something went wrong. Please try again later or contact support if the issue persists. Error details: ${t.message}`,position:"topRight"})}}function g(t){const e=`
-  <li id="${t._id}">
-    <img src="${t.book_image}" alt="${t.title}">
-    <h3>${t.title}</h3>
-    <p>${t.description}</p>
-    <p>${t.author}</p>
-    <p>${t.publisher}</p>
-    <a href="${t.amazon_product_url}" target="_blank">Buy on Amazon</a>
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                             */import{P as g}from"./assets/vendor-e8675f53.js";const n=JSON.parse(localStorage.getItem("books"))||[],p=document.getElementById("tui-pagination-container"),s=document.querySelector(".shopping-list"),o=new g(p,{totalItems:n.length,itemsPerPage:3,visiblePages:3,page:1});console.log(o);o.on("afterMove",function(e){c(e.page)});function c(e){s.innerHTML="";const t=(e-1)*o._options.itemsPerPage,i=t+o._options.itemsPerPage;n.slice(t,i).forEach(a=>{l(a)})}function l(e){const t=`
+  <li id="${e._id}">
+    <img src="${e.book_image}" alt="${e.title}">
+    <h3>${e.title}</h3>
+    <p>${e.description}</p>
+    <p>${e.author}</p>
+    <p>${e.publisher}</p>
+    <a href="${e.amazon_product_url}" target="_blank">Buy on Amazon</a>
     <button type="button">Remove from shopping list</button>
-  </li>`;n.insertAdjacentHTML("beforeend",e)}function m(t){if(t.target.nodeName!=="BUTTON")return;const e=t.target.parentNode,i=e.id,r=o.findIndex(a=>a._id===i);r!==-1&&(o.splice(r,1),localStorage.setItem("books",JSON.stringify(o))),e.remove()}n.addEventListener("click",m);l();
+  </li>`;s.insertAdjacentHTML("beforeend",t)}function d(e){if(e.target.nodeName!=="BUTTON")return;const t=e.target.parentNode,i=t.id,r=n.findIndex(a=>a._id===i);r!==-1&&(n.splice(r,1),localStorage.setItem("books",JSON.stringify(n)),o.reset(n.length),c(o.getCurrentPage())),t.remove()}s.addEventListener("click",d);c(1);
 //# sourceMappingURL=commonHelpers2.js.map
