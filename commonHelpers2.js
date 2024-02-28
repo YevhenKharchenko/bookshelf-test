22896ab06d3c3927d8d5b24cdbfabfd261f84ec6
YevhenKharchenko <143255199+YevhenKharchenko@users.noreply.github.com>
2024-02-28 06:22:35 +0000

    Deploying to gh-pages from @ YevhenKharchenko/bookshelf-test@54f6195f7ba07c6361dce59a4c5b3cee2e30859a 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                             */import{P as p}from"./assets/vendor-64d08f96.js";const n=JSON.parse(localStorage.getItem("books"))||[],g=document.querySelector(".tui-pagination"),s=document.querySelector(".shopping-list"),o=new p(g,{totalItems:n.length,itemsPerPage:3,visiblePages:3,page:1});o.on("afterMove",function(e){c(e.page)});function c(e){s.innerHTML="";const t=(e-1)*o._options.itemsPerPage,i=t+o._options.itemsPerPage;n.slice(t,i).forEach(a=>{l(a)})}function l(e){const t=`
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{P as p}from"./assets/vendor-64d08f96.js";const n=JSON.parse(localStorage.getItem("books"))||[],g=document.querySelector(".tui-pagination"),a=document.querySelector(".shopping-list"),o=new p(g,{totalItems:n.length,itemsPerPage:3,visiblePages:3,page:1,centerAlign:!0});o.on("afterMove",function(e){c(e.page)});function c(e){a.innerHTML="";const t=(e-1)*o._options.itemsPerPage,i=t+o._options.itemsPerPage;n.slice(t,i).forEach(s=>{l(s)})}function l(e){const t=`
   <li id="${e._id}">
     <img src="${e.book_image}" alt="${e.title}">
     <h3>${e.title}</h3>
@@ -7,5 +7,5 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                
     <p>${e.publisher}</p>
     <a href="${e.amazon_product_url}" target="_blank">Buy on Amazon</a>
     <button type="button">Remove from shopping list</button>
-  </li>`;s.insertAdjacentHTML("beforeend",t)}function d(e){if(e.target.nodeName!=="BUTTON")return;const t=e.target.parentNode,i=t.id,r=n.findIndex(a=>a._id===i);r!==-1&&(n.splice(r,1),localStorage.setItem("books",JSON.stringify(n)),o.reset(n.length),c(o.getCurrentPage())),t.remove()}s.addEventListener("click",d);c(1);
+  </li>`;a.insertAdjacentHTML("beforeend",t)}function d(e){if(e.target.nodeName!=="BUTTON")return;const t=e.target.parentNode,i=t.id,r=n.findIndex(s=>s._id===i);r!==-1&&(n.splice(r,1),localStorage.setItem("books",JSON.stringify(n)),o.reset(n.length),c(o.getCurrentPage())),t.remove()}a.addEventListener("click",d);c(1);
 //# sourceMappingURL=commonHelpers2.js.map
